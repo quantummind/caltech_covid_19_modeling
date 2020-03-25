ea706f9fb49ef40e0a1110641e643902ee6cddf7
Connor Soohoo <csoohoo@caltech.edu>
2020-03-24 19:54:34 -0700

    specify python version to python3 in download script

--- code/processing/raw_data_processing/daily_refresh.sh
+++ code/processing/raw_data_processing/daily_refresh.sh
@@ -2,6 +2,6 @@
 
 cd $(git rev-parse --show-toplevel)
 cd code/processing/raw_data_processing
-python get_JH_data.py
-python get_JH_daily.py
+python3 get_JH_data.py
+python3 get_JH_daily.py
 ./get_us_county_covid_data.sh
