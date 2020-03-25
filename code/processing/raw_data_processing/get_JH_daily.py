54452d13af72fc28d306388388f6fb9e0ac98fb2
Alexander <alexander.z2718@gmail.com>
2020-03-24 17:05:56 -0700

    reorganized us data

--- code/processing/raw_data_processing/get_JH_daily.py
+++ code/processing/raw_data_processing/get_JH_daily.py
@@ -49,7 +49,7 @@ for condition, url in urls.items():
 # Find home directory for repo
 repo = git.Repo("./", search_parent_directories=True)
 homedir = repo.working_dir
-datadir = f"{homedir}/data/"
+datadir = f"{homedir}/data/us/"
 
 dfs = list(output_dfs.values())
 dfs = pd.concat(dfs)
