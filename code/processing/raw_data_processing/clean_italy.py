eedcfba60696f6045d6b981149eea9819a3a6eb6
Alexander <alexander.z2718@gmail.com>
2020-03-27 14:41:01 -0700

    italy

--- code/processing/raw_data_processing/clean_italy.py
+++ code/processing/raw_data_processing/clean_italy.py
@@ -16,33 +16,38 @@ for i in range(len(files_reg)):
     sets_reg.append(dfr)
     dfr.to_csv(datadir + 'dati-regioni-en/' + files_reg[i][files_reg[i].rfind('/'):], index=False)
     
+pd.concat(sets_reg).to_csv(datadir + 'dati-regioni-en/dpc-covid19-ita-regioni-total.csv', index=False)
+    
 # translate provincial files and save in separate folder
 sets_prov = []
+dfs_prov = []
 files_prov = sorted(glob.glob(datadir + 'dati-province/dpc-covid19-ita-province-2*.csv'))
 for i in range(len(files_reg)):
     dfp = pd.read_csv(files_prov[i])
     dfp.columns = ["Date","Country", "Regional Code", "Region", "Province Code,","Province","ProvinceInitials","Latitude","Longitude","TotalCases","Note_IT","Note_ENG"]
-    sets_prov.append(dfp)
     indDrop = dfp[dfp['Latitude'] == 0].index
     dfp.drop(indDrop, inplace=True)
+    sets_prov.append(dfp)
     dfp.to_csv(datadir + 'dati-province-en/' + files_prov[i][files_prov[i].rfind('/'):], index=False)
     
-# compile all regional data into one timeseries of cases by region
-comp_reg = sets_reg[0][["Country", "Regional Code", "Region", "Latitude","Longitude"]]
-for i in range(len(files_reg)):
-    time = sets_reg[i]['Date'][0]
-    time = time[:time.find('T')]
-    time = time.replace("-", "")
-    headers = ["HospitalizedWithSymptoms","IntensiveCare","TotalHospitalized","HomeIsolation","TotalCurrentlyPositive","NewCurrentlyPositive","DischargedHealed","Deaths","TotalCases","Tested"]
-    for h in range(len(headers)):
-        comp_reg[time+headers[h]] = sets_reg[i][headers[h]]
-comp_reg.to_csv(datadir + 'dati-regioni-en/dpc-covid19-ita-regioni-total.csv', index=False)
+pd.concat(sets_prov).to_csv(datadir + 'dati-province-en/dpc-covid19-ita-province-total.csv', index=False)
 
-# compile all province data into one timeseries of cases by province
-comp_prov = sets_prov[0][["Country","Regional Code", "Region", "Province Code,","Province","ProvinceInitials","Latitude","Longitude"]]
-for i in range(len(files_reg)):
-    time = sets_prov[i]['Date'][0]
-    time = time[:time.find('T')]
-    time = time.replace("-", "")
-    comp_prov[time+'Cases'] = sets_prov[i]['TotalCases']
-comp_prov.to_csv(datadir + 'dati-province-en/dpc-covid19-ita-province-total.csv', index=False)
+# # compile all regional data into one timeseries of cases by region
+# comp_reg = sets_reg[0][["Country", "Regional Code", "Region", "Latitude","Longitude"]]
+# for i in range(len(files_reg)):
+#     time = sets_reg[i]['Date'][0]
+#     time = time[:time.find('T')]
+#     time = time.replace("-", "")
+#     headers = ["HospitalizedWithSymptoms","IntensiveCare","TotalHospitalized","HomeIsolation","TotalCurrentlyPositive","NewCurrentlyPositive","DischargedHealed","Deaths","TotalCases","Tested"]
+#     for h in range(len(headers)):
+#         comp_reg[time+headers[h]] = sets_reg[i][headers[h]]
+# comp_reg.to_csv(datadir + 'dati-regioni-en/dpc-covid19-ita-regioni-total.csv', index=False)
+# 
+# # compile all province data into one timeseries of cases by province
+# comp_prov = sets_prov[0][["Country","Regional Code", "Region", "Province Code,","Province","ProvinceInitials","Latitude","Longitude"]]
+# for i in range(len(files_reg)):
+#     time = sets_prov[i]['Date'][0]
+#     time = time[:time.find('T')]
+#     time = time.replace("-", "")
+#     comp_prov[time+'Cases'] = sets_prov[i]['TotalCases']
+# comp_prov.to_csv(datadir + 'dati-province-en/dpc-covid19-ita-province-total.csv', index=False)
