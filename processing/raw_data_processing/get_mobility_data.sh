b0c6f093031ecc590162e9aa6e30e0a1ff6589ec
Connor Soohoo <csoohoo@caltech.edu>
2020-04-03 08:56:57 -0700

    fix descartes mobility data

--- processing/raw_data_processing/get_mobility_data.sh
+++ processing/raw_data_processing/get_mobility_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $(git rev-parse --show-toplevel)
-wget https://github.com/descarteslabs/DL-COVID-19/blob/master/DL-us-m50.csv -O data/us/mobility/DL-us-m50.csv
-wget https://github.com/descarteslabs/DL-COVID-19/blob/master/DL-us-m50_index.csv -O data/us/mobility/DL-us-m50_index.csv
-wget https://github.com/descarteslabs/DL-COVID-19/blob/master/DL-us-mobility-daterow.csv -O data/us/mobility/DL-us-mobility-daterow.csv
-wget https://github.com/descarteslabs/DL-COVID-19/blob/master/DL-us-samples.csv -O data/us/mobility/DL-us-samples.csv
+wget https://raw.githubusercontent.com/descarteslabs/DL-COVID-19/master/DL-us-m50.csv -O data/us/mobility/DL-us-m50.csv
+wget https://raw.githubusercontent.com/descarteslabs/DL-COVID-19/master/DL-us-m50_index.csv -O data/us/mobility/DL-us-m50_index.csv
+wget https://raw.githubusercontent.com/descarteslabs/DL-COVID-19/master/DL-us-mobility-daterow.csv -O data/us/mobility/DL-us-mobility-daterow.csv
+wget https://raw.githubusercontent.com/descarteslabs/DL-COVID-19/master/DL-us-samples.csv -O data/us/mobility/DL-us-samples.csv
