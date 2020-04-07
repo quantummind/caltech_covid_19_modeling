53cbfb1a7f660521f3133f5a21973e20b24d7941
Thomas Hoffmann <7075559+thohoff@users.noreply.github.com>
2020-04-07 15:15:56 -0700

    Fixed get_ny_times data for doing differencing

--- processing/raw_data_processing/get_nyt_data.sh
+++ processing/raw_data_processing/get_nyt_data.sh
@@ -2,3 +2,5 @@
 cd $(git rev-parse --show-toplevel)
 wget https://github.com/nytimes/covid-19-data/raw/master/us-states.csv -O data/us/covid/nyt_us_states.csv
 wget https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv -O data/us/covid/nyt_us_counties.csv
+cd processing/raw_data_processing
+python3 difference_nyt_data.py
